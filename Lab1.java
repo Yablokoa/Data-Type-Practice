4e0010e9414be64f81e233490a90ed0dce7ea992
Yablokoa <avawiggins39@gmail.com>
2024-01-25 11:34:59 -0500

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,444 @@
+public class Lab1 {
+    public static void main(String[] args) {
+        //DOUBLES
+        //Double Double
+        //Declaration and initilazation
+        double num1 = 1e7;
+        double num2 = 1e8;
+        
+        Double difference = num1 - num2;
+        Double sum = num1 + num2;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double Double " + " " + num1 +  " " + "and" + " " +  num2 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum + "," + " "
+        +"difference = " + difference + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //Double char
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        char var = 'a';
+        
+        Double difference1 = num1 - var;
+        Double sum1 = num1 + var;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double Char " + " " + num1 + " " +  "and" + " " +  var + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum1 + "," + " "
+        +"difference = " + difference1 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //Double Short
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        short srt = 10000;
+        
+        Double difference2 = num1 - srt;
+        Double sum2 = num1 + srt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double Short " + " " + num1 + " " +  "and" + " " +  srt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum2 + "," + " "
+        +"difference = " + difference2 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //Double Byte
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        byte byt = 10;
+        
+        Double difference3 = num1 - byt;
+        Double sum3 = num1 + byt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double Byte " + " " + num1 + " " +  "and" + " " +  byt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum3 + "," + " "
+        +"difference = " + difference3 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //Double int
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        int num = -40000;
+        
+        Double difference4 = num1 - num;
+        Double sum4 = num1 + num;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double int " + " " + num1 + " " +  "and" + " " +  num + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum4 + "," + " "
+        +"difference = " + difference4 + "\n");
+        
+        //------------------------------------------------------------------------------------------------
+        //Double long
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        long lng = 50000000000L;
+        
+        Double difference5 = num1 - lng;
+        Double sum5 = num1 + lng;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double Long " + " " + num1 + " " +  "and" + " " +  lng + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum5 + "," + " "
+        +"difference = " + difference5 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //Double float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        float flt = -324.533f;
+        
+        Double difference6 = num1 - flt;
+        Double sum6 = num1 + flt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Double Float " + " " + num1 + " " +  "and" + " " +  flt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum6 + "," + " "
+        +"difference = " + difference6 + "\n");
+        
+        //CHAR
+        //------------------------------------------------------------------------------------------------
+        //char char
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        char ch1 = 'a';
+        char ch2 = 'b';
+      
+        
+        int difference7 = ch1 - ch2;
+        int sum7= ch1 + ch2;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Char Short " + " " + ch1 + " " +  "and" + " " +  ch2 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum7 + "," + " "
+        +"difference = " + difference7 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //char short
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        int difference8 = ch1 - srt;
+        int sum8= ch1 + srt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Char Short " + " " + ch1 + " " +  "and" + " " +  srt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum8 + "," + " "
+        +"difference = " + difference8 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //char byte
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        int difference9 = ch1 - byt;
+        int sum9= ch1 + byt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Char Byte " + " " + ch1 + " " +  "and" + " " +  byt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum9 + "," + " "
+        +"difference = " + difference9 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //char int
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        int difference10 = ch1 - num;
+        int sum10= ch1 + num;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Char int " + " " + ch1 + " " +  "and" + " " +  num + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum10 + "," + " "
+        +"difference = " + difference10 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //char long
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        long difference11 = ch1 - lng;
+        long sum11= ch1 + lng;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Char Long " + " " + ch1 + " " +  "and" + " " +  lng + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum11 + "," + " "
+        +"difference = " + difference11 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //char float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        float difference12 = ch1 - flt;
+        float sum12= ch1 + flt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Char Float " + " " + ch1 + " " +  "and" + " " +  flt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum12 + "," + " "
+        +"difference = " + difference12 + "\n");
+
+        //SHORT
+        //------------------------------------------------------------------------------------------------
+        //short byte
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        float difference13 = srt - byt;
+        float sum13= srt + byt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Short Byte " + " " + srt + " " +  "and" + " " +  byt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum13 + "," + " "
+        +"difference = " + difference13 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //short int
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        float difference14 = srt - num;
+        float sum14= srt + num;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Short int " + " " + srt + " " +  "and" + " " +  num + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum14 + "," + " "
+        +"difference = " + difference14 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //short long
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        float difference15 = srt - lng;
+        float sum15= srt + lng;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Short Long " + " " + srt + " " +  "and" + " " +  lng + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum15 + "," + " "
+        +"difference = " + difference15 + "\n");
+
+         //------------------------------------------------------------------------------------------------
+        //short float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        float difference16 = srt - flt;
+        float sum16= srt + flt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Short Float " + " " + srt + " " +  "and" + " " +  flt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum16 + "," + " "
+        +"difference = " + difference16 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //short short
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        short srt2 = 32767;
+        float difference17 = srt - srt2;
+        float sum17= srt + srt2;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Short Short " + " " + srt + " " +  "and" + " " +  srt2 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum17 + "," + " "
+        +"difference = " + difference17 + "\n");
+
+
+
+        //BYTE
+
+        //------------------------------------------------------------------------------------------------
+        //byte byte
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        byte byt2 = 127;
+    
+        float difference18 = byt - byt2;
+        float sum18= byt + byt2;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Byte Byte " + " " + byt + " " +  "and" + " " +  byt2 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum18 + "," + " "
+        +"difference = " + difference18 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //byte int
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+    
+        int difference19 = byt - num;
+        int sum19= byt + num;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Byte int " + " " + byt + " " +  "and" + " " +  num + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum19 + "," + " "
+        +"difference = " + difference19 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //byte long
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+    
+        long difference20 = byt - lng;
+        long sum20 = byt + lng;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Byte Long " + " " + byt + " " +  "and" + " " +  lng + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum20 + "," + " "
+        +"difference = " + difference20 + "\n");
+
+         //------------------------------------------------------------------------------------------------
+        //byte float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+    
+        float difference21 = byt - flt;
+        float sum21 = byt + flt;
+
+        //printig sum
+        System.out.println(
+        "\nSum and differences of Byte Float " + " " + byt + " " +  "and" + " " +  flt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum21 + "," + " "
+        +"difference = " + difference21 + "\n");
+
+        //INT
+
+         //------------------------------------------------------------------------------------------------
+        //int int
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        int num20 = -244413;
+
+        int difference22 = num - num20;
+        int sum22 = num + num20;
+
+        
+        //printig sum
+        System.out.println(
+        "\nSum and differences of int int " + " " + num + " " +  "and" + " " +  num20 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum22 + "," + " "
+        +"difference = " + difference22 + "\n");
+
+        //------------------------------------------------------------------------------------------------
+        //int long
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+
+
+        long difference23 = num - lng;
+        long sum23 = num + lng;
+
+        
+        //printig sum
+        System.out.println(
+        "\nSum and differences of int long " + " " + num + " " +  "and" + " " +  lng + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum23 + "," + " "
+        +"difference = " + difference23 + "\n");
+
+
+        //------------------------------------------------------------------------------------------------
+        //int float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+
+
+        float difference24 = num - flt;
+        float sum24 = num + flt;
+
+        
+        //printig sum
+        System.out.println(
+        "\nSum and differences of int float " + " " + num + " " +  "and" + " " +  flt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum24 + "," + " "
+        +"difference = " + difference24 + "\n");
+
+
+        //LONG
+
+        //------------------------------------------------------------------------------------------------
+        //long long
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        long lng2 = 40000000000L;
+
+        long difference25 = lng - lng2;
+        long sum25 = lng + lng2;
+
+        
+        //printig sum
+        System.out.println(
+        "\nSum and differences of int long " + " " + lng + " " +  "and" + " " +  lng2 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum25 + "," + " "
+        +"difference = " + difference25 + "\n");
+
+
+        //------------------------------------------------------------------------------------------------
+        //long float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+
+
+        float difference26 = lng - flt;
+        float sum26 = lng + flt;
+
+        
+        //printig sum
+        System.out.println(
+        "\nSum and differences of int float " + " " + lng + " " +  "and" + " " +  flt + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum26 + "," + " "
+        +"difference = " + difference26 + "\n");
+
+        //FLOAT
+
+        //------------------------------------------------------------------------------------------------
+        //float float
+        //Declaration and initilazation
+        System.out.println("------------------------------------------------------------------------------------------------");
+        
+        float flt2 = 10;
+
+        float difference27 = flt - flt2;
+        float sum27 = flt + flt2;
+
+        
+        //printig sum
+        System.out.println(
+        "\nSum and differences of int float " + " " + flt + " " +  "and" + " " +  flt2 + " " + "is" + ":" + " " + "\n"
+         + "sum = " + sum27 + "," + " "
+        +"difference = " + difference27 + "\n");
+
+
+
+    }
+}
+
